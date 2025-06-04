022dfe0c870478081a0b14aa84ff141550250231
Aulirma <24051204005@mhs.unesa.ac.id>
2025-06-04 18:22:58 +0700

    Update urls.py
    perbaiki letak search schedules

--- busgo/bus/urls.py
+++ busgo/bus/urls.py
@@ -5,9 +5,9 @@ app_name = 'bus'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search_schedules, name='search_schedules'),
     path('<slug:operator_slug>/', views.operator_detail, name='operator_detail'),
     path('schedule/<int:schedule_id>/', views.ticket_detail, name='ticket_detail'),
-     path('book/<int:schedule_id>/', views.book_ticket, name='book_ticket'),
+    path('book/<int:schedule_id>/', views.book_ticket, name='book_ticket'),
     path('booking-success/<int:booking_id>/', views.booking_success, name='booking_success'),
-    path('search/', views.search_schedules, name='search_schedules'),
 ]
